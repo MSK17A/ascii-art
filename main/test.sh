63c46cc4047b078fc93091f756b17eebc14e68a4
Mohammed AlSammak <msk170a@gmail.com>
2023-06-07 20:32:07 +0300

    added test bash file

--- main/test.sh
+++ main/test.sh
@@ -2,7 +2,45 @@
 
 #* test file for multiple diff cases
 
+go run . "\"test\""
+
+echo
+
+go run . "HELLO"
+
+echo
+
+go run . "HeLlo HuMaN"
+
+echo
+
+go run . "1Hello 2There"
+
+echo
+
+
+go run . "Hello\nThere"
+
+echo
+
+go run . "{Hello & There #}"
+
+echo
+
+go run . 'hello There 1 to 2!' 
+
+echo
+
 go run . "MaD3IrA&LiSboN"
+
+echo
+
+go run . "1a\"#FdwHywR&/()="
+
+echo
+
+
+go run . "{|}~"
 
 echo 
 
@@ -10,13 +48,19 @@ go run . "1a\"#FdwHywR&/()="
 
 echo
 
-go run . "{|}~"
+
+go run . ":;<=>?@"
 
 echo
 
 go run . "[\]^_ 'a"
 
 echo
+
+go run . '\!" #$%&'"'"'()*+,-./'
+
+echo
+
 
 go run . "RGB"
 
