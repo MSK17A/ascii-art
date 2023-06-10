8aad5fbe6b971925b7db59ea8f9f94f51f9e35e5
Mohammed AlSammak <msk170a@gmail.com>
2023-06-11 00:38:51 +0300

    Code cleaning (conditions checks)

--- main/main.go
+++ main/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Print()
+	/*if len(os.Args) < 2 {
+		fmt.Println("Usage: [OPTION] <apply options to this> <input string> [BANNER]")
+		return
 	} else if len(os.Args) >= 2 && len(os.Args) < 3 {
 		if os.Args[1] == "" {
-			fmt.Print()
+			fmt.Println("Usage: [OPTION] <apply options to this> <input string> [BANNER]")
+			return
 		} else {
 			asciiART.PrintART("",os.Args[1], "standard", "")
 		}
@@ -30,5 +32,21 @@ func main() {
 		} else {
 			asciiART.PrintART("",os.Args[1], os.Args[2], "")
 		}
+	}*/
+
+	// Flags
+	colorname := flag.String("color", "", "coloring")
+	flag.Parse()
+
+	if len(os.Args) == 2 {
+		asciiART.PrintART("",os.Args[1], "standard", "")
+	} else if *colorname !="" && len(os.Args) == 3 {
+		asciiART.PrintART("",os.Args[2], "standard", *colorname)
+	} else if *colorname !="" && len(os.Args) == 4 {
+		asciiART.PrintART(os.Args[2],os.Args[3], "standard", *colorname)
+	} else if *colorname !="" && len(os.Args) == 5 {
+		asciiART.PrintART(os.Args[2],os.Args[3], os.Args[4], *colorname)
+	} else {
+		fmt.Println("Usage: [OPTION] <apply options to this> <input string> [BANNER]")
 	}
 }
