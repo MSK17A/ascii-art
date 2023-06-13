bf3c5d9decb8d4eabe1da76352171b89253b3571
Mohammed AlSammak <msk170a@gmail.com>
2023-06-13 23:25:37 +0300

    outputs beginning and end positions

--- containsstring.go
+++ containsstring.go
@@ -7,19 +7,19 @@ package asciiART
 		input_str:		the letter to be tested
 
 	output:
-		indicies		positions of each found word (first char pos)
+		indicies		positions of each found word [beg, end]
 		sub_len			length of the sub-string
 
 */
 
-func ContainsString(sub_str string, input_str string) ([]int, int) {
+func ContainsString(sub_str string, input_str string) ([][]int, int) {
 	//index := strings.Index(input_str, sub_str)
-	var indicies []int
+	var indicies [][]int
 	sub_len := len(sub_str)
 
 	for idx := 0; idx <= len(input_str) && idx+sub_len <= len(input_str); idx++ {
 		if sub_str == input_str[idx:idx+sub_len] {
-			indicies = append(indicies, idx)
+			indicies = append(indicies, []int{idx, idx + sub_len})
 		}
 	}
 	return indicies, sub_len
